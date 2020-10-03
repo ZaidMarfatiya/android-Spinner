67566786e922e26d7fe975a6338327a74d565b01
Hardik-Parmar007 <72206656+Hardik-Parmar007@users.noreply.github.com>
2020-10-03 11:15:33 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.spinnerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+
+        String items[] = new String[]{"Android","Python","PHP","C","Java","C++"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,items);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                String name = spinner.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "Selected Item is : "+name, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView)
+            {
+
+            }
+        });
+    }
+}
